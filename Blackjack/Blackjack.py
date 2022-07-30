d2d47a5bac15569cbf773fb881679fcf07be8932
Robert <mail.robertvari@gmail.com>
2022-07-30 17:52:52 +0200

    update

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,6 +1,17 @@
+from game_assets.cards import Deck
+
+
 class Blackjack:
     def __init__(self):
         self.__intro()
+
+        self._deck = Deck()
+
+        # todo create players
+        # todo start game
+        # todo get winner
+        # todo restart game
+        # todo exit game
 
     @staticmethod
     def __intro():
