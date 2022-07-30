a07dd7cd3fbaa32bba830321ac4a71152502b3e8
Robert <mail.robertvari@gmail.com>
2022-07-30 14:24:29 +0200

    update

---
+++ 01_define_a_class.py
@@ -0,0 +1,14 @@
+class Dice:
+    # class attributes
+    sides = 6
+    color = "white"
+
+
+print(Dice.sides)
+print(Dice.color)
+
+Dice.sides = 20
+Dice.color = "red"
+
+print(Dice.sides)
+print(Dice.color)
