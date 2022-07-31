73ba4ed3a485f10f4e2209077260cd9add69ddd6
Robert <mail.robertvari@gmail.com>
2022-07-31 14:29:02 +0200

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -15,13 +15,18 @@ class PlayerBase:
     def _create(self):
         first_names = ["Brittney", "Curtis", "Lucas", "Chip", "Simon"]
         last_names = ["Moriah", "Tristin", "Troy", "Gale", "Lynn"]
+        self._name = f"{random.choice(first_names)} {random.choice(last_names)}"
+        self._credits = random.randint(10, 100)
 
     def __str__(self):
         return f"Name: {self._name}\nCredits: {self._credits}\nCards: {self._hand}"
 
 
 class Player(PlayerBase):
-    pass
+    def _create(self):
+        # calls Base Class _create() to get everything from there.
+        super()._create()
+        self._name = input("What is your name?")
 
 
 class AIPlayer(PlayerBase):
@@ -32,5 +37,5 @@ if __name__ == '__main__':
     player = Player()
     ai_player = AIPlayer()
 
-    player.say_hello()
-    ai_player.say_hello()
+    print(player)
+    print(ai_player)
